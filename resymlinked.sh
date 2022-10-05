2c5ae4aaa0e2495c07bbeda917ceb795d13ee380
早坂 彪流 <ta-hayasaka@PC105728.local>
2022-10-05 15:34:57 +0900

    fixed

---
+++ resymlinked.sh
@@ -0,0 +1,9 @@
+#!bin/sh
+
+ln -nfs $HOME/.zprezto/runcoms/zshrc .zshrc
+ln -nfs $HOME/.zprezto/runcoms/zlogin .zlogin
+ln -nfs $HOME/.zprezto/runcoms/zlogout .zlogout
+ln -nfs $HOME/.zprezto/runcoms/zpreztorc .zpreztorc
+ln -nfs $HOME/.zprezto/runcoms/zprofile .zprofile
+ln -nfs $HOME/.zprezto/runcoms/zshenv .zshenv
+ln -nfs $HOME/.zprezto/runcoms/zshrc .zshrc
